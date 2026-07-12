6d1dd4406889bb5d17674ee00ad3526039d40fa2
jennifervdm <jennifer.van.der.meer@student.nhlstenden.com>
2026-07-12 14:43:13 +0200

    Create tabel_1_dataset.R

---
+++ assets/tabel_1_dataset.R
@@ -0,0 +1,12 @@
+*Tabel 1. Patiënten uit onderzochte dataset, afkomstig van het onderzoek van [Platzer et al. (2019)](<https://doi.org/10.1371/journal.pone.0219698>). De ruwe sequencing data in FASTQ bestanden is afkomstig van 8 vrouwen, 4 met RA (leeftijden 54-66), vastgesteld voor >12 maanden en positief getest op autoantistoffen ACPA. En een negatief geteste controlegroep van 4 (leeftijden 15-42).*
+  |     ID      | Age |   Sex   |               Status                |
+  | ----------- | --- | ------- | ----------------------------------- |
+  | SRR4785819  | 31  | female  |               Normal                |
+  | SRR4785820  | 15  | female  |               Normal                |
+  | SRR4785828  | 31  | female  |               Normal                |
+  | SRR4785831  | 42  | female  |               Normal                |
+  | SRR4785979  | 54  | female  | Rheumatoid arthritis (established)  |
+  | SRR4785980  | 55  | female  | Rheumatoid arthritis (established)  |
+  | SRR4785986  | 60  | female  | Rheumatoid arthritis (established)  |
+  | SRR4785988  | 59  | female  | Rheumatoid arthritis (established)  |
+  
